bf88d63264f345152d5b98148666c1ae10df4c31
Christopher Viquez Aguilar <cris.567.22.viquez@gmail.com>
2024-06-20 20:30:23 -0600

    Subo todos los comandos de la instruccion

--- Lab#6_A.sql
+++ Lab#6_A.sql
@@ -1,3 +1,53 @@
 USE C12565
 
-SELECT * FROM LLEVA
+-- Escenario 1: Nivel de aislamiento read uncommitted
+
+-- 6
+set implicit_transactions off;
+set transaction isolation level read uncommitted;
+begin transaction t1; 
+PRINT @@TRANCOUNT
+Select avg(Nota) from Lleva;
+
+-- 8
+Select avg(Nota) from Lleva;
+
+-- 10
+Select avg(Nota) from Lleva;
+Commit transaction t1;
+
+-- Escenario 2: Nivel de aislamiento read committed
+
+-- 12
+set implicit_transactions off;
+set transaction isolation level read committed;
+begin transaction t3;
+Select avg(Nota) from Lleva;
+
+-- 14
+Select max(Nota) from Lleva;
+
+-- 16
+Commit transaction t3;
+
+-- Escenario 3: Nivel de aislamiento repeatable read
+
+-- 18
+set implicit_transactions off;
+set transaction isolation level repeatable read;
+begin transaction t5;
+Select avg(Nota) from Lleva;
+
+-- 20
+Select avg(Nota) from Lleva;
+commit transaction t5;
+
+-- 22
+set implicit_transactions off;
+set transaction isolation level serializable;
+begin transaction t7;
+Select avg(Nota) from Lleva;
+
+-- 24
+Select avg(Nota) from Lleva;
+commit transaction t7;
--- Lab#6_B.sql
+++ Lab#6_B.sql
@@ -1,2 +1,47 @@
 USE C12565
 
+-- Escenario 1: Nivel de aislamiento read uncommitted
+
+-- 7
+set implicit_transactions off;
+begin transaction t2;
+PRINT @@TRANCOUNT
+Select * from sys.sysprocesses 
+where open_tran = 1;
+Update Lleva 
+set Nota = Nota*(0.8)
+where Nota is not null;
+
+-- 9
+rollback transaction t2;
+
+-- Escenario 2: Nivel de aislamiento read committed
+
+-- 13
+set implicit_transactions off;
+begin transaction t4;
+Update Lleva 
+set Nota = Nota*(0.8)
+where Nota is not null;
+
+-- 15
+Select * from sys.sysprocesses
+where open_tran = 1 
+commit transaction t4;
+
+-- Escenario 3: Nivel de aislamiento repeatable read
+
+-- 19
+set implicit_transactions off;
+begin transaction t6;
+Insert into Lleva(CedEstudiante, SiglaCurso,NumGrupo, Semestre, Anno, Nota)
+values('12345', 'CI0127', 1, 1,2019, 85);
+
+-- 23
+set implicit_transactions off;
+begin transaction t8;
+Insert into Lleva(CedEstudiante, SiglaCurso,NumGrupo, Semestre, Anno, Nota)
+values('12345', 'CI0127', 1, 1,2019, 85);
+
+-- 25
+commit transaction t8;
